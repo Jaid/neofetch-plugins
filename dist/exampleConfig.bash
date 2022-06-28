0c6124c205b25f8082b9968ee6a8f6884dea6611
Jaid <jaid.jsx@gmail.com>
2022-06-28 16:16:32 +0200

    Rebuilt
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- dist/exampleConfig.bash
+++ dist/exampleConfig.bash
@@ -11,6 +11,33 @@ get_containers_healthy() {
     containers_healthy="No docker command"
     return
   fi
+  names=$(docker container ls --all --filter "status=running" --format "{{.Names}}")
+  count=$(printf %s "$names" | wc -l)
+  if [ -z "$count" ] || [ "$count" == 0 ]; then
+    return
+  fi
+  actualCount=$((count + 1))
+  if [ "$nerd_font" == "on" ]; then
+    containers_healthy="$(color 14)$actualCount  $(color fg)$names"
+  else
+    containers_healthy="$(color 14)[$actualCount] $(color fg)$names"
+  fi
+
+}
+
+get_containers_healthy() {
+
+  nerd_font="${nerd_font:=off}"
+  while [[ "$1" ]]; do
+    case $1 in
+    "--nerd_font") nerd_font="$2" ;;
+    esac
+  done
+
+  if [ ! -x "$(command -v docker)" ]; then
+    containers_healthy="No docker command"
+    return
+  fi
   names=$(docker container ls --all --filter "status=running" --filter "health=healthy" --format "{{.Names}}")
   count=$(printf %s "$names" | wc -l)
   if [ -z "$count" ] || [ "$count" == 0 ]; then
@@ -109,6 +136,7 @@ get_containers_stopped() {
 print_info() {
   info model
   info underline
+  info "Containers running" containers_running # https://github.com/Jaid/neofetch-plugins/blob/main/dist/plugins/containers_running.bash
   info "Containers healthy" containers_healthy # https://github.com/Jaid/neofetch-plugins/blob/main/dist/plugins/containers_healthy.bash
   info "Containers unhealthy" containers_unhealthy # https://github.com/Jaid/neofetch-plugins/blob/main/dist/plugins/containers_unhealthy.bash
   info "Containers paused" containers_paused # https://github.com/Jaid/neofetch-plugins/blob/main/dist/plugins/containers_paused.bash
--- dist/plugins.bash
+++ dist/plugins.bash
@@ -11,6 +11,33 @@ get_containers_healthy() {
     containers_healthy="No docker command"
     return
   fi
+  names=$(docker container ls --all --filter "status=running" --format "{{.Names}}")
+  count=$(printf %s "$names" | wc -l)
+  if [ -z "$count" ] || [ "$count" == 0 ]; then
+    return
+  fi
+  actualCount=$((count + 1))
+  if [ "$nerd_font" == "on" ]; then
+    containers_healthy="$(color 14)$actualCount  $(color fg)$names"
+  else
+    containers_healthy="$(color 14)[$actualCount] $(color fg)$names"
+  fi
+
+}
+
+get_containers_healthy() {
+
+  nerd_font="${nerd_font:=off}"
+  while [[ "$1" ]]; do
+    case $1 in
+    "--nerd_font") nerd_font="$2" ;;
+    esac
+  done
+
+  if [ ! -x "$(command -v docker)" ]; then
+    containers_healthy="No docker command"
+    return
+  fi
   names=$(docker container ls --all --filter "status=running" --filter "health=healthy" --format "{{.Names}}")
   count=$(printf %s "$names" | wc -l)
   if [ -z "$count" ] || [ "$count" == 0 ]; then
